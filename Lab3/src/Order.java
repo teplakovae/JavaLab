9c60cc4dbb734ed663f896ce032bf40967eae0a8
teplakovae <teplakovae281@gmail.com>
2025-12-16 17:50:56 +0300

    Update Order.java

--- Lab3/src/Order.java
+++ Lab3/src/Order.java
@@ -1,35 +1,51 @@
+import java.util.concurrent.atomic.AtomicInteger;
 /**
  * Класс заказа в ресторане.
  * Содержит информацию о блюде, клиенте и статусе готовности.
  */
+
 public class Order {
-    private static int NEXT_ID = 1;
+    private static final AtomicInteger NEXT_ID = new AtomicInteger(1);
 
     private final int id;
     private final RestaurantConstants.Dish dish;
     private final String customerName;
     private final Client client;
 
-    private boolean ready = false;
+    private volatile boolean ready = false;
 
     public Order(RestaurantConstants.Dish dish,
                  String customerName,
                  Client client) {
 
-        this.id = NEXT_ID++;
+        this.id = NEXT_ID.getAndIncrement();
         this.dish = dish;
         this.customerName = customerName;
         this.client = client;
     }
 
-    public int getId() { return id; }
-    public RestaurantConstants.Dish getDish() { return dish; }
-    public String getCustomerName() { return customerName; }
-    public Client getClient() { return client; }
+    public RestaurantConstants.Dish getDish() {
+        return dish;
+    }
 
-    public boolean isReady() { return ready; }
-    public void setReady(boolean ready) { this.ready = ready; }
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    public void markReady() {
+        this.ready = true;
+    }
 }
-
-
-
