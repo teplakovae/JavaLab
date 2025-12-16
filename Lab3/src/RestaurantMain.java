a1fc472d065ca91703827291c7a1df59903c8bbb
teplakovae <teplakovae281@gmail.com>
2025-12-16 17:51:58 +0300

    Update RestaurantMain.java

--- Lab3/src/RestaurantMain.java
+++ Lab3/src/RestaurantMain.java
@@ -7,13 +7,14 @@ import java.util.concurrent.*;
  */
 
 public class RestaurantMain {
+    public static double totalIncome = 0;
     public static void main(String[] args) {
         System.out.println("Restaurant Claude Monet Opened\n");
-
         BlockingQueue<Order> kitchenQueue = new LinkedBlockingQueue<>();
         BlockingQueue<ClientRequest> clientQueue = new LinkedBlockingQueue<>();
         List<Order> readyOrders = Collections.synchronizedList(new ArrayList<>());
 
+        // Создание и запуск официантов
         List<Waiter> waiters = new ArrayList<>();
         for (int i = 1; i <= RestaurantConstants.WAITERS_COUNT; i++) {
             Waiter waiter = new Waiter(i, kitchenQueue, readyOrders, clientQueue);
@@ -21,12 +22,14 @@ public class RestaurantMain {
             waiter.start();
         }
 
+        // Создание кухни с пулом поваров
         Kitchen kitchen = new Kitchen(
                 RestaurantConstants.INITIAL_CHEFS_COUNT,
                 kitchenQueue,
                 readyOrders
         );
 
+        // Создание и запуск клиентов с задержкой
         List<Client> clients = new ArrayList<>();
         for (int i = 1; i <= RestaurantConstants.MAX_CUSTOMERS; i++) {
             Client client = new Client(i, clientQueue);
@@ -38,6 +41,7 @@ public class RestaurantMain {
             client.start();
         }
 
+        // Ожидание всех клиентов
         for (Client c : clients) {
             try { c.join(); }
             catch (InterruptedException ignored) {}
@@ -45,20 +49,23 @@ public class RestaurantMain {
 
         System.out.println("All clients have been served.");
 
+        // Ждем, пока кухня и официанты закончат обработку заказов
         while (!kitchenQueue.isEmpty() || !readyOrders.isEmpty()) {
             try { Thread.sleep(200); } catch (InterruptedException ignored) {}
         }
 
+        // Завершаем работу официантов
         for (Waiter waiter : waiters) waiter.interrupt();
         for (Waiter waiter : waiters) {
             try { waiter.join(); } catch (InterruptedException ignored) {}
         }
 
+        // Завершаем работу кухни
         kitchen.shutdown();
 
-        System.out.println("\nRestaurant Claude Monet Closed");
+        System.out.printf("\nRestaurant Claude Monet Closed. Total income: $%.2f\n", totalIncome);
+
     }
 }
 
 //На основе сериала "Кухня" :)
-
