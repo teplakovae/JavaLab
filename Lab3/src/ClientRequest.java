6d45b522268d6c575862d3779f41b570b0cfc2a7
teplakovae <teplakovae281@gmail.com>
2025-12-16 17:49:59 +0300

    Update ClientRequest.java

--- Lab3/src/ClientRequest.java
+++ Lab3/src/ClientRequest.java
@@ -2,13 +2,20 @@
  * Класс для передачи заказа от клиента официанту.
  */
 public class ClientRequest {
-    public final Client client;
-    public final RestaurantConstants.Dish dish;
+    private final Client client;
+    private final RestaurantConstants.Dish dish;
+
+    public Client getClient() {
+        return client;
+    }
+
+    public RestaurantConstants.Dish getDish() {
+        return dish;
+    }
+
 
     public ClientRequest(Client client, RestaurantConstants.Dish dish) {
         this.client = client;
         this.dish = dish;
     }
 }
-
-
