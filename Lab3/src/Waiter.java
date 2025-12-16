2bba4822af68a75ff218985ba529bf71f0cf825d
teplakovae <teplakovae281@gmail.com>
2025-12-16 17:52:30 +0300

    Update Waiter.java

--- Lab3/src/Waiter.java
+++ Lab3/src/Waiter.java
@@ -29,13 +29,13 @@ public class Waiter extends Thread {
 
         try {
             while (!Thread.currentThread().isInterrupted()) {
-
+                // Берем новый запрос клиента
                 ClientRequest req = clientQueue.take();
 
                 System.out.println(getName() + ": Taking order from "
-                        + req.client.getName() + " for " + req.dish.getName());
-
-                Order order = new Order(req.dish, req.client.getName(), req.client);
+                        + req.getClient().getName() + " for " + req.getDish().getName());
+                // Создаем заказ для кухни
+                Order order = new Order(req.getDish(), req.getClient().getName(), req.getClient());
                 kitchenQueue.put(order);
 
                 waitForOrder(order);
@@ -51,18 +51,33 @@ public class Waiter extends Thread {
 
     private void waitForOrder(Order order) throws InterruptedException {
         synchronized (readyOrders) {
-            while (!order.isReady()) {
+            while (!readyOrders.contains(order)) {
                 readyOrders.wait();
             }
             readyOrders.remove(order);
         }
+
     }
 
     private void deliverOrder(Order order) {
         System.out.println(getName() + ": Delivering "
                 + order.getDish().getName() + " to " + order.getCustomerName());
+        double tip = order.getClient().getTip();
+        double income = order.getDish().getPrice() + tip;
 
+        // Синхронизируем доступ к общему доходу
+        synchronized (RestaurantMain.class) {
+            RestaurantMain.totalIncome += income;
+        }
+        System.out.printf("%s: Delivering %s to %s | Dish: $%.2f, Tip: $%.2f, Total so far: $%.2f\n",
+                getName(),
+                order.getDish().getName(),
+                order.getCustomerName(),
+                order.getDish().getPrice(),
+                tip,
+                RestaurantMain.totalIncome);
+
+        // Сообщаем клиенту, что заказ доставлен
         order.getClient().serveDish();
     }
 }
-
